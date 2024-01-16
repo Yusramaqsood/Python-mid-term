fa1b542e12846c5df5485fe51506c3fbecb38ace
Yusramaqsood <156826089+Yusramaqsood@users.noreply.github.com>
2024-01-16 21:05:12 +0500

    Add files via upload

---
+++ yusra.py
@@ -0,0 +1,22 @@
+print("calculator")
+num1=int(input("enter your first number:"))
+num2=int(input("enter your second number:"))
+print ("press + for addition /n press - for subtraction /n press * for multiplication press / for division")
+choice=input("enter your choice for  + - * /")
+if choice == "+":
+    print(num1+num2)
+
+elif choice == "-":
+    print(num1-num2)
+
+elif choice == "*":
+    print(num1*num2)
+
+elif choice == "/":
+    print(num1/num2)
+    
+
+
+
+    
+           
